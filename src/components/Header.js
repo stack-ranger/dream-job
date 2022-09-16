3a197c9abaa6b78ae0a4869d5dcbcd2fac66dc00
Aksel Uhr <akseluhr8@gmail.com>
2022-09-16 16:01:19 +0200

    Created Navbar Component

---
+++ src/components/Header.js
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import homeIcon from "../../public/Home_Icon.svg";
+import userIcon from "../../public/User_Icon.svg";
+import signOutIcon from "../../public/Sign_InOut_Icon.svg";
+import signUpIcon from "../../public/Sign_Up_Icon.svg";
+
+// TODO: make collapse
+const Topbar = (props) => {
+  // Dummy auth token for now, chane later
+  const [isSignedIn, setIsSignedIn] = useState(true);
+
+  return (
+    <nav
+      className="
+        relative
+        flex flex-wrap
+        items-center
+        justify-between
+        bg-transparent
+        text-white
+        navbar navbar-expand-lg navbar-light
+        "
+    >
+      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <div
+          className="collapse navbar-collapse flex-grow items-center"
+          id="navbarSupportedContent"
+        >
+          <div className="flex">
+            <h1 className="text-2xl pt-3 pb-3 pl-6">DREAM JOB</h1>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <div className="flex">
+            <div className="text-center cursor-pointer  pr-5">
+              <Image
+                src={homeIcon}
+                className=""
+                alt="Log in Icon"
+                width={25}
+                height={25}
+              />
+              <span className="text-xs  mt-1">HOME</span>
+            </div>
+            <div className="text-center cursor-pointer pr-5">
+              <Image
+                src={isSignedIn ? userIcon : signUpIcon}
+                className=""
+                alt="Mode action"
+                width={25}
+                height={25}
+              />
+              <span className="text-xs  mt-1">
+                {isSignedIn ? "PROFILE" : "SIGN UP"}
+              </span>
+            </div>
+            <div className="text-center cursor-pointer ">
+              <Image
+                src={signOutIcon}
+                className=""
+                alt="Mode action"
+                width={25}
+                height={25}
+              />
+              <span className="text-xs  mt-1">
+                {isSignedIn ? "SIGN OUT" : "SIGN IN"}
+              </span>
+            </div>
+            <button
+              className="ml-2 text-xs px-5 py-2 bg-white border border-rounded text-black"
+              onClick={() => setIsSignedIn((prevCheck) => !prevCheck)}
+            >
+              change dummy state
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Topbar;
