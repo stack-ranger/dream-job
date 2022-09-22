3c6b815fba05da8d8d425d09f9a2fa0cbd7e7ad8
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-09-22 23:36:18 +0200

    Make string matching more fuzzy to get more matches #6

--- src/server/router/skills.ts
+++ src/server/router/skills.ts
@@ -15,12 +15,12 @@ export const skillRouter = createRouter()
 			number: z.number().min(1).max(100).nullish(),
 		}),
 		async resolve({ input }) {
-			const roleCleaned = input.role.replace(/ /g, "%");
+			const roleCleaned = "%"  + input.role.replace(" ", "%") + "%";
 			const queryString = Prisma.sql`SELECT s.name, COUNT(j.id) as skill_count
 								FROM "public"."Skill" s
 								JOIN "public"."JobSkill" js ON js.skill_name = s.name
 								JOIN "public"."Job" j ON j.id  = js.job_id
-								WHERE LOWER(j.role) LIKE LOWER(${roleCleaned})  OR LOWER(j.text) LIKE LOWER(${roleCleaned})
+								WHERE LOWER(j.role) LIKE LOWER(${roleCleaned}) OR LOWER(j.text) LIKE LOWER(${roleCleaned})
 								GROUP BY s.name
 								ORDER BY skill_count desc 
 								LIMIT ${input.number || 20};`
