39193bca00c98328da7f8f22549cac02ae91f69c
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-09-25 22:17:39 +0200

    Created job search to find relevant skills #35

---
+++ src/pages/Skills.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {InferGetStaticPropsType} from "next";
+import SkillProvider from "~/context/skillContext";
+import SkillPlot from "~/components/skillPlot/skillPlot";
+import JobSearchPresenter from "~/components/jobSearch/jobSearchPresenter";
+
+const Skills = ({jobTitles}: InferGetStaticPropsType<typeof getStaticProps>) => {
+
+    return (
+        <div className="flex-col">
+            <SkillProvider>
+                <div className="pt-12 flex justify-center">
+                    <JobSearchPresenter jobList={jobTitles}/>
+                </div>
+                <div className="pt-12 flex justify-center">
+                    <SkillPlot height={500} width={500}/>
+                </div>
+            </SkillProvider>
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    // TODO Expand list of fetch common titles from database
+    const commonJobTitles = [
+        "Web Developer",
+        "Software Engineer",
+        "Software Developer",
+        "Frontend Developer",
+        "Network Engineer",
+        "Java Developer",
+        "Web Developer",
+        "Junior Developer",
+        "Full Stack Developer",
+        "Software Architect",
+        "Cloud Engineer",
+    ]
+    return {
+        props: {
+            jobTitles: commonJobTitles
+        }
+    }
+}
+
+export default Skills;
+
