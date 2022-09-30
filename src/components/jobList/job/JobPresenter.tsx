ba1750fb235f34c7bdd02a84a19fd74fccb60584
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-09-30 22:47:29 +0200

    Added logo or placeholder to job cards #38

--- src/components/jobList/job/JobPresenter.tsx
+++ src/components/jobList/job/JobPresenter.tsx
@@ -1,6 +1,18 @@
 import JobView from "./JobView";
+import {useState} from "react";
+
+const imgLoader = ({ src }: {src: string}) => {
+    const cleanName = src.replace(/ /g, '-');
+    return `https://findwork-dev-images.s3.amazonaws.com/${cleanName}`;
+}
+
+const getRandomColor = () => {
+    const randColors = ["bg-green-400", "bg-red-400", "bg-blue-400", "bg-orange-400"];
+    return randColors[Math.floor(Math.random() * randColors.length)];
+}
 
 const JobPresenter = ({job}: {job: {role: string, company_name: string}}) => {
-    return (<JobView role={job.role} company_name={job.company_name}/>);
+    const [placeholder, setPlaceholder] = useState(false);
+    return (<JobView role={job.role} company_name={job.company_name} placeholder={placeholder} imgLoader={imgLoader} getRandomColor={getRandomColor} imgError={()=>setPlaceholder(true)}></JobView>);
 }
 export default JobPresenter;
--- src/components/jobList/job/JobView.tsx
+++ src/components/jobList/job/JobView.tsx
@@ -1,8 +1,36 @@
-const JobView = ({role, company_name}: { role: string, company_name: string }) => {
+import Image from 'next/image'
+
+
+const JobView = ({
+                     role,
+                     company_name,
+                     imgLoader,
+                     imgError,
+                     placeholder,
+                     getRandomColor
+                 }:
+                     {
+                         role: string,
+                         company_name: string,
+                         imgLoader: ({src}: { src: string }) => string,
+                         imgError: () => void,
+                         placeholder: boolean
+                         getRandomColor: () => string | undefined
+                     }) => {
     return (
         <div
             className="w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
             <div className="flex flex-col items-center pb-10">
+                <div className="my-3">
+                    {placeholder ?
+                        <div
+                            className={`${getRandomColor()} flex items-center justify-center w-20 h-20 text-3xl font-bold text-white rounded-full`}>
+                            {company_name.charAt(0)}
+                        </div>
+                        : <Image className="rounded-full"
+                                 loader={imgLoader} src={company_name} alt={company_name} width={80} height={80}
+                                 onError={imgError}/>}
+                </div>
                 <h5 className="mb-1 text-xl font-medium text-gray-900">{role}</h5>
                 <span className="text-sm text-gray-500">{company_name}</span>
             </div>
