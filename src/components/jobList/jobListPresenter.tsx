6cc95953a86438dcf3de98f32e6b602b22952bb6
Riccardo Romagnoli <riccardo.romagnoli@stochastic.ai>
2022-10-08 16:50:04 +0200

    keep skills for latest job search

--- src/components/jobList/jobListPresenter.tsx
+++ src/components/jobList/jobListPresenter.tsx
@@ -7,16 +7,17 @@ import { useRouter } from "next/router"
 import {trpc} from "~/utils/trpc";
 
 const JobListPresenter = () => {
-    const { jobs, loading, skills, setSkills } = useJobStore();
+    const { jobs, loading } = useJobStore();
+    const [currentSkillSearch, setCurrentSkillSearch] = useState<string[]>([])
     const router = useRouter()
     
     useEffect(() => {
       const tmp = router?.query?.skills
       const skills: string[] = Array.isArray(tmp) ? tmp : tmp ? [tmp] : []
-      setSkills(skills)
+      setCurrentSkillSearch(skills)
     }, [router.query.skills])
 
-    return (<JobListView jobs={jobs} loading={loading} skills={skills}/>);
+    return (<JobListView jobs={jobs} loading={loading} skills={currentSkillSearch}/>);
 }
 
 
