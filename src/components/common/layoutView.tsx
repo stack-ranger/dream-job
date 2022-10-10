5d6a576491be575ff47dca9eb7f925b81303e446
Riccardo Romagnoli <riccardo.romagnoli@stochastic.ai>
2022-10-10 21:28:46 +0200

    job button UI: save, delete, logged

--- src/components/common/layoutView.tsx
+++ src/components/common/layoutView.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
+import { ToastContainer } from 'react-toastify'
 import Header from '~/components/common/Header/headerPresenter'
+import "react-toastify/dist/ReactToastify.css";
 
 const LayoutView = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <ToastContainer/>
     </>
   )
 }
