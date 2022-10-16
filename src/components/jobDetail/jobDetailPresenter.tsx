9232296dce2ccd46dea6558dda824d8d4c4bcc0a
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-10-16 23:26:59 +0200

    Fix: check is user is logged in to save job #57

--- src/components/jobDetail/jobDetailPresenter.tsx
+++ src/components/jobDetail/jobDetailPresenter.tsx
@@ -1,10 +1,13 @@
 import { trpc } from '~/utils/trpc'
 import JobDetailView from '~/components/jobDetail/jobDetailView'
 import useJobStore from '~/stores/jobStore'
+import { toast } from 'react-toastify'
 import { useState } from 'react'
 import { JobInterface } from '~/types/job'
+import { useSession } from 'next-auth/react'
 
 const JobDetailPresenter = ({ jobId }: { jobId: string }) => {
+  const { status } = useSession()
   const { jobs, skills, deleteJob, saveJob } = useJobStore()
   // look for the job in the store
   const [job, setJob] = useState(jobs.find((job) => job.id === jobId) as JobInterface)
@@ -12,15 +15,20 @@ const JobDetailPresenter = ({ jobId }: { jobId: string }) => {
   const { isLoading } = trpc.useQuery(['jobs.byId', { id: jobId }], { enabled: !job, onSuccess: setJob })
 
   const matchedSkills = job ? job.skills.filter((skill: string) => skills.includes(skill)) : []
-  const bookmarkJob = job?.saved
-    ? async () => {
+
+  const bookmarkJob = () => {
+    if (status === 'authenticated') {
+      if (job?.saved) {
         deleteJob(job.id)
         setJob({ ...job, saved: false })
-      }
-    : async () => {
+      } else {
         saveJob(job.id)
         setJob({ ...job, saved: true })
       }
+    } else {
+      toast.warn('Login to save a job')
+    }
+  }
 
   return (
     <JobDetailView
