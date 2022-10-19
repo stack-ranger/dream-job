ef9c2865ba21d1acf3c0144aeae33a9c9602d9d8
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-10-20 00:21:38 +0200

    Added footer #4

--- src/components/homePage/homePageView.tsx
+++ src/components/homePage/homePageView.tsx
@@ -5,6 +5,7 @@ import JobSearchPresenter from '../jobSearch/jobSearchPresenter'
 import SkillPlot from '../skillPlot/skillPlot'
 import SkillSearchPresenter from '../skillSearch/skillSearchPresenter'
 import IconAnimationPresenter from '~/components/homePage/iconAnimation/iconAnimationPresenter'
+import Footer from '~/components/common/Footer/FooterView'
 
 interface HomePageViewProps {
   searchSelected: 'job' | 'skill'
@@ -23,17 +24,15 @@ export default function HomePageView({
 }: HomePageViewProps) {
   return (
     <>
-      <div className="flex justify-center">
-        <div className="flex flex-col py-16">
-          <h1 className="pb-5 text-center font-black text-4xl md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            The stack matters!
-          </h1>
-          <h2 className="text-center text-lg lg:text-xl font-bold text-gray-500 dark:text-gray-400">
-            {searchSelected === 'job'
-              ? 'Tell us with which stack want to work with and we will find the jobs for you.'
-              : 'Tell us about the job you are interested in and we will find the most relevant stack for you.'}
-          </h2>
-        </div>
+      <div className="flex flex-col py-16">
+        <h1 className="pb-5 text-center font-black text-4xl md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          The stack matters!
+        </h1>
+        <h2 className="text-center text-lg lg:text-xl font-bold text-gray-500 dark:text-gray-400">
+          {searchSelected === 'job'
+            ? 'Tell us with which stack want to work with and we will find the jobs for you.'
+            : 'Tell us about the job you are interested in and we will find the most relevant stack for you.'}
+        </h2>
       </div>
       <div className="flex flex-row justify-center">
         <div className="inline-flex rounded-md shadow-sm h-10" role="group">
