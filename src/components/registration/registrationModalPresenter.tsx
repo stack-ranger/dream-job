450335d054cdd3e00083c8edf72ca2db4782fe17
Aksel Uhr <akseluhr8@gmail.com>
2022-10-21 21:28:15 +0200

    small cleanup

--- src/components/registration/registrationModalPresenter.tsx
+++
@@ -1,2 +0,0 @@
-const registrationPresenter = "hi"
-export default registrationPresenter
--- src/pages/Profile.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ProfilePresenter from '../components/profile/ProfilePresenter'
-
-const Profile = () => {
-  return <ProfilePresenter />
-}
-
-export default Profile
